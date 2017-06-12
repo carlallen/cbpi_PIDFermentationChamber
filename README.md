b680335f2b7c81722aa323daac0924eb636e4f3d
Carl Allen <carl@allenofmn.com>
2017-06-11 22:44:08 -0500

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# craftbeerpi_PIDFermentationChamber
-PID setpoint Fermentation Chamber plugin for craftbeerpi3
+# PID Setpoint Fermentation Chamber Logic for CraftBeerPi
+
+Use this Controller Logic to automatically control the setpoint for a fermentation chamber
+
+## Installation
+
+Download and Install this Plugin via the CraftBeerPi user interface
+or pull into the [craftbeerpi]/modules/plugins/ directory
+
+## Fermenter Configuration
+
+Sensor1: Beer Temperature
+Sensor2: Chamber Temperature
+Sensor3: Not Used
+
+Compressor Delay: The time in minutes to wait between compressor cycles
+Kp: The is the proportional gain for the PID (Default 2)
+Ki: The the integral gain for the PID (Default 0.0001)
+Kd: The the derivative gain for the PID (Default 2)
---
+++ __init__.py
@@ -0,0 +1,97 @@
+from modules import cbpi
+from modules.core.controller import FermenterController
+from modules.core.props import Property
+import datetime
+
+@cbpi.fermentation_controller
+class PIDFermentationChamber(FermenterController):
+    p = Property.Number("Kp", True, 2)
+    i = Property.Number("Ki", True, 0.0001)
+    d = Property.Number("Kd", True, 2)
+    c_delay = Property.Number("Compressor Delay", True, 10, "minutes")
+
+    def stop(self):
+        super(FermenterController, self).stop()
+        self.heater_off()
+        self.cooler_off()
+
+    def run(self):
+        self.chamber_target_temp = self.get_target_temp()
+        self.compressor_wait = datetime.datetime.utcnow()
+        pid = ChamberSetpointPID(self.p, self.i, self.d)
+        while self.is_running():
+            temp = self.get_temp()
+            target_temp = self.get_target_temp()
+            chamber_temp = self.get_chamber_temp()
+            self.chamber_target_temp = target_temp + pid.update(temp, self.get_target_temp())
+
+            print "Chamber Target Temp: %s", self.chamber_target_temp
+            cbpi.save_to_file(self.fermenter_id, self.chamber_target_temp, prefix="chamber_target")
+
+            if chamber_temp is None:
+                self.heater_off()
+            elif temp < target_temp and chamber_temp < self.chamber_target_temp:
+                self.heater_on()
+            elif temp >= target_temp or chamber_temp >= self.chamber_target_temp:
+                self.heater_off()
+            else:
+                self.heater_off()
+
+            if chamber_temp is None:
+                self.cooler_off()
+            elif temp > target_temp and chamber_temp > self.chamber_target_temp and self.compressor_wait < datetime.datetime.utcnow():
+                self.cooler_on()
+            elif temp <= target_temp or chamber_temp <= self.chamber_target_temp:
+                self.cooler_off()
+            else:
+                self.cooler_off()
+
+            self.sleep(1)
+
+    @cbpi.try_catch(None)
+    def get_chamber_temp(self):
+        return self.get_sensor_value(int(self.get_fermenter().sensor2))
+
+    @cbpi.try_catch(None)
+    def cooler_on(self):
+        f = self.get_fermenter()
+        if f.cooler is not None:
+            self.actor_on(id=int(f.cooler))
+
+    @cbpi.try_catch(None)
+    def cooler_off(self):
+        self.compressor_wait = datetime.datetime.utcnow() + datetime.timedelta(minutes=int(self.c_delay))
+        f = self.get_fermenter()
+        if f.cooler is not None:
+            self.actor_off(int(f.cooler))
+
+    @cbpi.try_catch(None)
+    def get_fermenter(self):
+        self.api.cache.get("fermenter").get(self.fermenter_id)
+
+class ChamberSetpointPID(object):
+
+    def __init__(self, kp, ki, kd):
+        self.k_p = float(kp)
+        self.k_i = float(ki)
+        self.k_d = float(kd)
+        self.last_error = 0
+        self.integration = 0
+        self.last_output = 0
+
+    def update(self, temp, setpoint):
+
+        error = setpoint - temp
+        if self.last_error == 0:
+            self.last_error = error #catch first run error
+
+        P_value = self.k_p * error
+        D_value = -(self.k_d * (error - self.last_error))
+        self.last_error = error
+        if self.last_output > -15 and self.last_output < 15:
+            self.integration = self.integration + error
+
+        I_value = self.integration * self.k_i
+
+        self.last_output = max(min(P_value + I_value + D_value, 15), -15)
+        return self.last_output
